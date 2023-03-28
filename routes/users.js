bf7abfd8de505de8fdbe696410336a4f76638c2a
yogesh-naidu <yogeshnaidu33@gmail.com>
2023-03-28 02:30:59 -0500

    modified users file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var sum = 0;
+var more = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more+= 1;
+  sum+= more;
+  res.send('sum is: ' + sum);
 });
 
 module.exports = router;
